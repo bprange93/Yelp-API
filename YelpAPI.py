f8a4a205ce793fab59d160565aa58e32c2731bd6
Brandon Prange <bprange93@gmail.com>
2021-09-10 15:41:14 -0500

    API call works but still running into module issues. Added workspace to possibly add request script. Will be looking into other frameworks to see if Flask or Django works better for Python over Node.js.

--- YelpAPI.py
+++ YelpAPI.py
@@ -1,4 +1,5 @@
 import requests
 
-response = requests.get("https://api.yelp.com/v3/businesses/search")
-print(response.json('the stillery'))
+response = requests.get(
+    "https://api.yelp.com/v3/businesses/l7mNzyV3pOZ3m87jRY6J7A/reviews")
+print(response.json())
